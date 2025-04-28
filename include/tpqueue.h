6c3bedacb172021253bcbe50c0532f591c811f8e
Lix <zelenovaaleksandra2@gmail.com>
2025-04-28 12:14:54 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,15 +1,55 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
-
-template<typename T>
-class TPQueue {
-  // реализация шаблона очереди с приоритетом на связанном списке
-};
-
+#include <iostream>
 struct SYM {
   char ch;
   int prior;
 };
-
+template <typename T>
+class TPQueue {
+private:
+  struct Node {
+    T data;
+    Node* next;
+    Node(T val) : data(val), next(nullptr) {}
+  };
+  Node* head;
+public:
+  TPQueue() : head(nullptr) {}
+  ~TPQueue() {
+    while (head) {
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+    }
+  }
+  void enqueue(T value) {
+    Node* newNode = new Node(value);
+    if (!head || head->data.prior < value.prior) {
+      newNode->next = head;
+      head = newNode;
+    } else {
+      Node* current = head;
+      while (current->next && current->next->data.prior >= value.prior) {
+        current = current->next;
+      }
+      newNode->next = current->next;
+      current->next = newNode;
+    }
+  }
+  T dequeue() {
+    if (!head) {
+      throw std::runtime_error("Queue is empty");
+    }
+    Node* temp = head;
+    head = head->next;
+    T value = temp->data;
+    delete temp;
+    return value;
+  }
+  bool isEmpty() const {
+    return head == nullptr;
+  }
+};
 #endif  // INCLUDE_TPQUEUE_H_
